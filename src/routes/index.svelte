2f0d5341ab069a31241929280f0df807ff7c22e1
abaihaaqi <nizarbaihaq@gmail.com>
2021-10-12 07:43:49 +0700

    First commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,29 @@
+<div class="flex justify-center items-center h-screen flex-col bg-gradient-to-b from-blue-300">
+  <img src="../../grei.png" alt="Foto grei yang ganteng" class="rounded-full w-80 border-8 shadow-md">
+  <h1 class="font-bold uppercase text-6xl pt-5">aku greialdi</h1>
+  <h3 class="text-2xl pt-3">Panggil aku grei</h3>
+</div>
+<div class="pt-8">
+  <h2 class="text-4xl text-center p-6 mx-auto font-serif">Aku adalah konsultan desain grafis</h2>
+  <h3 class="text-xl text-center">Keahlianku adalah menggambar di Illustrator, Photoshop, Figma, dan canva</h3>
+  <object type="image/svg+xml" data="image.svg" title="logo" class="flex gap-6 pt-4 justify-center">
+    <img src="../../illustrator.svg" alt="Logo canva" class=" w-16"/>
+    <img src="../../photoshop.svg" alt="Logo canva" class="w-16"/>
+    <img src="../../figma.svg" alt="Logo canva" class="w-8"/>
+    <img src="../../canva.svg" alt="Logo canva" class="w-16"/>
+  </object>
+  <h4 class="text-xl text-center pt-8">aku bisa menjadikan wajah kalian yang burik menjadi ganteng seperti oppa korea</h4>
+  <div class="flex justify-center items-center gap-24 pt-6">
+    <img src="../../emo-alay.jpg" alt="Jamet" class="rounded shadow-md h-56">
+    <i class="fas fa-angle-right text-5xl"></i>
+    <img src="../../oppa-hyunnie.jpg" alt="Oppa Hyunnie" class="rounded shadow-md h-56">
+  </div>
+  <h4 class="text-xl text-center pt-4">Kalian bisa lihat hasil karyaku di bawah</h4>
+</div>
+<div class="h-screen">
+</div>
+<h1 class="font-bold text-center uppercase text-6xl pt-5">yahaha kena prank</h1>
+<h1 class="font-bold text-center uppercase text-6xl pt-5">hayukkkk</h1>
+<div class="bg-black p-8 mt-8">
+  <h4 class="text-xl text-center text-gray-400 pt-4">greialdy @ 2021 - Buatan tangan seorang teman</h4>
+</div>
