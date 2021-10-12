6fd0e30bea4ddd2c0d59935982b5bb12a1eef99c
abaihaaqi <nizarbaihaq@gmail.com>
2021-10-12 08:23:54 +0700

    another toml change

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
 import preprocess from "svelte-preprocess";
-import adapter from '@sveltejs/adapter-netlify';
+import netlify from '@sveltejs/adapter-netlify';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
-    adapter: adapter(),
+    adapter: netlify(),
     target: "#svelte",
   },
 
